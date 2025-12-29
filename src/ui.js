b1c4832ced987ab4b9e39894214bcf002dc1d755
Ryan <ryan_nelson45@aol.com>
2025-12-29 03:19:49 -0500

    Add files via upload

--- src/ui.js
+++ src/ui.js
@@ -40,7 +40,6 @@ export const ui = {
   finalHighScoreEl: $("#finalHighScore"),
   restartBtn: $("#restartBtn"),
   menuModal: $("#menuModal"),
-  menuResumeBtn: $("#menuResumeBtn"),
   menuNewRunBtn: $("#menuNewRunBtn"),
   menuRelicsBtn: $("#menuRelicsBtn"),
   menuHotkeysBtn: $("#menuHotkeysBtn"),
